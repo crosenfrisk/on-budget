0893b1b040928efd24742bbb33e2202186415af2
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-03 18:23:26 -0500

    change to <script> tag for idb.js hope that will help with index.html. Added service-workers.js

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
-router.post("/api/transaction", ({body}, res) => {
+router.post("/api/transaction/bulk", ({body}, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
       res.json(dbTransaction);
