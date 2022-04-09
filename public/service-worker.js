84ccb88ba4a7d8d9e8d3567b6b7081fb55fc3ba3
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-09 12:21:07 -0500

    added files to cache on server.js for proper running

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,7 +2,22 @@ const APP_PREFIX = "on-budget";
 const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
-const FILES_TO_CACHE = ["./index.html"];
+const FILES_TO_CACHE = [
+  "/",
+  "./index.html",
+  "./css/styles.css",
+  "./manifest.json",
+  "./js/idb.js",
+  "./service-worker.js",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
+];
 
 // Respond with cached resources
 self.addEventListener('fetch', function (e) {
