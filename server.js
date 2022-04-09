d28860fb85031a5f8e15a3f2cfc773d09c12882c
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-09 11:55:25 -0500

    working with server.js to figure out connection for herkoku/mongo atlas

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+  useNewUrlParser: true
 });
 
 // routes
