620ba08e63ad7ce7946182108280fec7ebcaa11b
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-04-09 10:59:01 -0500

    Added manifest.json with required and additional fields. Wanted to see what it was like to include a description, orientation, and various displays (standalone versus minimal-ui) will test in browswer when deploying to Heroku, may remove.

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,14 +1,14 @@
 // create variable to hold db connection
 let db;
 // establish a connection to IndexedDB database called 'budget' and set it to version 1
-const request = indexedDB.open("budget", 1);
+const request = indexedDB.open("onbudget", 1);
 
 // this event will emit if the database version changes (non-existent to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
   // save a reference to the database
   const db = event.target.result;
-  // create an object store (table) called `new_budget`, set it to have an auto incrementing primary key of sorts
-  db.createObjectStore("new_budget", { autoIncrement: true });
+  // create an object store (table) called `updated_budget`, set it to have an auto incrementing primary key of sorts
+  db.createObjectStore("updated_budget", { autoIncrement: true });
 };
 
 // upon successful
@@ -32,10 +32,10 @@ request.onerror = function (event) {
 // This function will be executed if we attempt to submit a new budget and there's no internet connection
 function saveRecord(record) {
   // open a new transaction with the database with read and write permissions
-  const transaction = db.transaction(["new_budget"], "readwrite");
+  const transaction = db.transaction(["updated_budget"], "readwrite");
 
-  // access the object store for `new_budget`
-  const budgetObjectStore = transaction.objectStore("new_budget");
+  // access the object store for `updated_budget`
+  const budgetObjectStore = transaction.objectStore("updated_budget");
 
   // add record to your store with add method
   budgetObjectStore.add(record);
@@ -43,10 +43,10 @@ function saveRecord(record) {
 
 function uploadBudget() {
   // open a transaction on your pending db
-  const transaction = db.transaction(["budget"], "readwrite");
+  const transaction = db.transaction(["updated_budget"], "readwrite");
 
   // access your pending object store
-  const budgetObjectStore = transaction.objectStore("budget");
+  const budgetObjectStore = transaction.objectStore("updated_budget");
 
   // get all records from store and set to a variable
   const getAll = budgetObjectStore.getAll();
@@ -54,7 +54,7 @@ function uploadBudget() {
   getAll.onsuccess = function () {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch("/api/transaction", {
+      fetch("/api/transaction/bulk", {
         method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
@@ -68,8 +68,8 @@ function uploadBudget() {
             throw new Error(serverResponse);
           }
 
-          const transaction = db.transaction(["budget"], "readwrite");
-          const budgetObjectStore = transaction.objectStore("budget");
+          const transaction = db.transaction(["updated_budget"], "readwrite");
+          const budgetObjectStore = transaction.objectStore("updated_budget");
           // clear all items in your store
           budgetObjectStore.clear();
         })
